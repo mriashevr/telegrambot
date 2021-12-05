c2a61bb38e347c728f9637eef8eda6ff5256934f
Софья Орлова <orlova.sofia2002@gmail.com>
2021-12-05 13:55:08 +0300

    refactor: changed methods

--- handler_setup.py
+++ handler_setup.py
@@ -1,4 +1,6 @@
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler, updater
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler, updater, \
+    CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from actions import *
 from textProceed import *
 
@@ -6,4 +8,6 @@ from textProceed import *
 def handlers_setup(dp):
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.text, fromHandlerToFunction))
+    dp.add_handler(CallbackQueryHandler(callback=keyboard_callback_handler))
+    #dp.add_handler(MessageHandler(Filters.voice, fromHandlerToFunction))
     return dp
